e3bd92f2afd2228d5a39333a269fa5c35662c662
MilliSay <millisay2013@gmail.com>
2013-12-07 04:11:46 -0500

    Created post model

--- millisay/readwrite/models.py
+++ millisay/readwrite/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.model) #a 1000 word post, or "MilliSay"
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    content = models.TextField()
+    upvotes = models.IntegerField(default=0)
+    downvotes = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
