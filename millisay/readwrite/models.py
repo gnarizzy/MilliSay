d3c34369ca22248dfb403451b3299534f5646dea
MilliSay <millisay2013@gmail.com>
2013-12-07 04:17:36 -0500

    Created database

--- millisay/readwrite/models.py
+++ millisay/readwrite/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Post(models.model) #a 1000 word post, or "MilliSay"
+class Post(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     content = models.TextField()
