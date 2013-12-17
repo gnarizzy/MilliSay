07f82768a5d4648683143bc23fdf9158ab797c12
MilliSay <millisay2013@gmail.com>
2013-12-16 19:02:34 -0500

    Added note to make default publication date the current time

--- millisay/readwrite/models.py
+++ millisay/readwrite/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField('date published') #make default now, set timzeone for east coast
     content = models.TextField()
     upvotes = models.IntegerField(default=0)
     downvotes = models.IntegerField(default=0)
