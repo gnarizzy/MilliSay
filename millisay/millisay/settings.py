317dd482a08a9b56566a9ccdd94a6e68c1d5a5c4
MilliSay <millisay2013@gmail.com>
2013-12-19 01:33:09 -0500

    enabled admin site

--- millisay/millisay/settings.py
+++ millisay/millisay/settings.py
@@ -127,9 +127,9 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'readwrite'
+    'readwrite',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
