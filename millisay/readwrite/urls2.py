1015e1bff653df7c645f6e33154125254f9aabb0
MilliSay <millisay2013@gmail.com>
2013-12-18 20:58:05 -0500

    added some comments

--- millisay/readwrite/urls2.py
+++ millisay/readwrite/urls2.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 from readwrite import views
-
+#This is pretty bad design, but it's the only way to get a clean submit url without making a whole new app
 urlpatterns = patterns('',
         url(r'^$', views.submit_post, name='submit'),
         )
