fb9eba728f7d8c3ad3243072759e5a145d4c1853
MilliSay <millisay2013@gmail.com>
2013-12-22 06:59:42 -0500

    Used bootstrap to create non-hideous post page

--- millisay/readwrite/forms.py
+++ millisay/readwrite/forms.py
@@ -3,7 +3,7 @@ from readwrite.models import Post
 
 class PostForm(forms.ModelForm):
     title = forms.CharField(max_length=200, help_text="Please enter a title.")
-    content = forms.CharField(help_text="Please enter your post's content.")
+    content = forms.CharField(widget=forms.Textarea, help_text="Please enter your post's content.")
 
     class Meta:
         model = Post
