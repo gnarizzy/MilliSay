3b4ee4a279091f2877ac97c07bcd6e4522ae77aa
Gautam <millisay2013@gmail.com>
2013-12-29 22:32:42 -0500

    Added navbar, fixed layout issues for posts

--- millisay/readwrite/forms.py
+++ millisay/readwrite/forms.py
@@ -4,7 +4,7 @@ from readwrite.models import Post
 class PostForm(forms.ModelForm):
     title = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}),max_length=200, help_text="Title")
     content = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control'}), help_text="Content")
-
+    #Create hidden field for word count?
     class Meta:
         model = Post
         fields = ('title', 'content')
