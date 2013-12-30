88db7874b47383f96f86c8d9658853084bd1942a
Gautam <millisay2013@gmail.com>
2013-12-30 03:18:06 -0500

    try to fix deployment bug by adding hidden field to form

--- millisay/readwrite/forms.py
+++ millisay/readwrite/forms.py
@@ -4,7 +4,8 @@ from readwrite.models import Post
 class PostForm(forms.ModelForm):
     title = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}),max_length=200, help_text="Title")
     content = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control'}), help_text="Content")
-    #Create hidden field for word count?
+    words = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
     class Meta:
         model = Post
-        fields = ('title', 'content')
+        fields = ('title', 'content','words')
