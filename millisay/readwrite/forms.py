c0d986faa057ebdd3f6be5113320e11e83aab4f3
Gautam <millisay2013@gmail.com>
2014-01-06 22:44:45 -0500

    Various minor changes

--- millisay/readwrite/forms.py
+++ millisay/readwrite/forms.py
@@ -1,11 +1,13 @@
 from django import forms
+import datetime
 from readwrite.models import Post
 
 class PostForm(forms.ModelForm):
     title = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}),max_length=200, help_text="Title")
     content = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control'}), help_text="Content")
     words = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    pub_date = forms.DateTimeField(widget=forms.HiddenInput(),initial=datetime.datetime.now())
 
     class Meta:
         model = Post
-        fields = ('title', 'content','words')
+        fields = ('title', 'content','words','pub_date')
--- millisay/readwrite/models.py
+++ millisay/readwrite/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.utils import timezone
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    pub_date = models.DateTimeField(default=timezone.now()) #make default now, set timzeone for east coast
+    pub_date = models.DateTimeField() #make default now, set timzeone for east coast
     content = models.TextField()
     upvotes = models.IntegerField(default=0)
     downvotes = models.IntegerField(default=0)
