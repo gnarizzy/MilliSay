bde03b766ea98b3334468d95ded9a6dcf33273be
Gautam <millisay2013@gmail.com>
2014-02-13 16:25:07 -0500

    Re-added publication date

--- millisay/readwrite/models.py
+++ millisay/readwrite/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    pub_date = models.DateTimeField() #make default now, set timzeone for east coast
+    pub_date = models.DateTimeField(auto_now_add=True) #make default now, set timzeone for east coast
     content = models.TextField()
     upvotes = models.IntegerField(default=0)
     downvotes = models.IntegerField(default=0)
