2fe268ac56c0202b8342433d5786c49bae128799
Gautam <gautam@uga.edu>
2016-03-09 01:14:58 -0500

    Deleted serializers.py since file wasn't used.

--- millisay/readwrite/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from readwrite.models import Post
-
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = ('id', 'title', 'pub_date','content', 'words', 'is_top')
